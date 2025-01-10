2a2b7605c0ad49c37f3839ac62c3c21fa3278954
Daniel Bergey <bergey@teallabs.org>
2025-01-10 11:22:29 -0500

    invoke ripgrep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use std::process::Command;
 
-fn main() -> std::io::Result<()> {
-    let output = Command::new("echo").arg("Hello world").output()?;
-    println!("{:?}", output);
+fn main() -> anyhow::Result<()> {
+    let directory = "/Users/bergey/braze/platform/develop/dashboard";
+    let regexen = Command::new("rg")
+        .arg("And\\(/([^\\n]*)/")
+        .arg("--only-matching")
+        .arg("--line-number")
+        .arg("--replace")
+        .arg("$1")
+        .arg(directory)
+        .output()?;
+
+    if !regexen.status.success() {
+        eprintln!("{:?}", regexen);
+        std::process::exit(1);
+    }
+    let mut counter: u64 = 0;
+    for line in std::str::from_utf8(&regexen.stdout)?.lines() {
+        counter += 1;
+    }
+    println!("{counter}");
+
     Ok(())
 }
